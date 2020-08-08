245e61e4e2152df9214729fff3db32d3c25b9111
zxq <1294333900@qq.com>
2020-08-08 08:53:03 +0800

    第二章：栈

---
+++ Stack/src/com/company/ArrayStack.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class ArrayStack<E> implements Stack<E> {
+
+    Array<E> array;
+
+    public ArrayStack(int capacity){
+        array = new Array<>(capacity);
+    }
+
+    public ArrayStack(){
+        array = new Array<>();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    public int getCapacity(){
+        return array.getCapacity();
+    }
+
+    @Override
+    public void push(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E pop() {
+        return array.removeLast();
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+}
