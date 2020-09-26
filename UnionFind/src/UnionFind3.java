879986acc859752d2f8170c433dfcc3b69c129a0
zxq <1294333900@qq.com>
2020-09-26 22:05:15 +0800

    第十一章：并查集

---
+++ UnionFind/src/UnionFind3.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+
+public class UnionFind3 implements UF {
+
+    // 第二版Union-Find
+
+    private int[] parent;
+    private int[] sz;
+
+    public UnionFind3(int size) {
+        parent = new int[size];
+        sz = new int[size];
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+
+    @Override
+    public int getSize() {
+        return parent.length;
+    }
+    // 查找过程，查找元素p所对应的集合编号
+    // o(h)复杂度，h为数的高度
+    private int find(int p) {
+        if (p < 0 && p >= parent.length) {
+            throw new IllegalArgumentException("p is out of bound");
+        }
+        while (p != parent[p]) {
+            p = parent[p];
+        }
+        return p;
+    }
+
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    @Override
+    public void unionElements(int p, int q) {
+
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) {
+            return;
+        }
+        if (sz[pRoot] < sz[qRoot]) {
+            parent[pRoot] = qRoot;
+            sz[qRoot] += sz[pRoot];
+        }else {
+            parent[qRoot] = pRoot;
+            sz[pRoot] += sz[qRoot];
+        }
+
+    }
+}
